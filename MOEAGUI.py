3229803f3b56f5e70262cdd0b02ad4716e2a0e61
YinsongYan <58545770+YinsongYan@users.noreply.github.com>
2024-04-11 20:38:14 +0800

    Update MOEAGUI.py

--- MOEAGUI.py
+++ MOEAGUI.py
@@ -66,7 +66,7 @@ if __name__ == '__main__':
     record_gpt_solution = True  # record the input and oupt of each run of gpt to learn a general linear operator
     model_LLM = "gpt-3.5-turbo"  # "model": "gpt-3.5-turbo",
     # "model": "gpt-4-0613",
-    key = "my key"  # your key
+    key = "fk225608-1rtTkwE6GRDI37PKBwLxd4Bmsu4f4j98"  # your key
     debug_mode = False
 
 
